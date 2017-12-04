8bb3f98c283baf2f0b8d1b002b51772d5a824c5c
Joshua Greaves <joshua.greaves@googlemail.com>
2017-12-04 14:44:24 -0700

    Initial Commit

---
+++ CartPole.py
@@ -0,0 +1,6 @@
+from DQN import DQN
+
+# Here we should pass in some function which creates our network
+dqn = DQN()
+
+# Then we create a train loop where we alternate gathering experience and calling dqn.train
---
+++ DQN.py
@@ -0,0 +1,18 @@
+
+
+class DQN(object):
+    def __init__(self):
+        # Create the DQN
+        pass
+
+    def train(self, s, a, r, s_prime, t):
+        pass
+
+    def qvals(self):
+        pass
+
+    def act_greedy(self):
+        pass
+
+    def act_epsilon(self, epsilon):
+        pass
