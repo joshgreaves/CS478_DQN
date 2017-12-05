799c285de0c789b989d501323d20c83761ce9899
nancyfulda <nancy_fulda@yahoo.com>
2017-12-05 12:58:27 -0700

    clip loss

--- DQN.py
+++ DQN.py
@@ -17,7 +17,7 @@ class DQN(object):
 		target = r + self.discount * np.max(current_net_q, axis=1)
 
 		loss_vector = target - pred
-		loss = np.sum(np.power(loss_vector, 2))
+		loss = np.sum(np.power(np.clip(loss_vector, -1, 1), 2))
 
 		# learning_net.backwards(loss) Something like this.. Whatever is normal for Tensorflow in backprop
 
