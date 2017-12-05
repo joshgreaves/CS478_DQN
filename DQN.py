0c43094891e0dff360179d87b129835f923afb7b
Joshua Greaves <joshua.greaves@googlemail.com>
2017-12-05 13:19:52 -0700

    Added loss function

--- DQN.py
+++ DQN.py
@@ -5,7 +5,7 @@ import random
 
 class DQN(object):
     def __init__(self, network_definition, state_dim, action_dim, gamma=0.95, copy_weight_interval=1000, epsilon=0.1,
-                 epsilon_decay=1.0):
+                 epsilon_decay=1.0, learning_rate=0.001):
         # Cache important info
         self._network = network_definition
         self._state_dim = state_dim
@@ -15,10 +15,11 @@ class DQN(object):
         self._current_update_countdown = copy_weight_interval
         self._epsilon = epsilon
         self._epsilon_decay = epsilon_decay
+        self._learning_rate = learning_rate
 
         # Create networks
         self._state = tf.placeholder(tf.float32, [None, self._state_dim])
-        self._action = tf.placeholder(tf.float32, [None, self._action_dim])
+        self._action = tf.placeholder(tf.float32, [None, self._action_dim])  # One hot
         self._reward = tf.placeholder(tf.float32, [None, 1])
         self._state_prime = tf.placeholder(tf.float32, [None, self._state_dim])
         self._terminal = tf.placeholder(tf.float32, [None, 1])
@@ -28,7 +29,16 @@ class DQN(object):
                                                   reuse=False)
         self._target_net = self._network.create(self._state_prime, self._state_dim, self._action_dim,
                                                 scope="TargetNet", reuse=False)
+        var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+        self._learning_vars = [var for var in var_list if "LearningNet" in var.name]
+        self._target_vars = [var for var in var_list if "TargetNet" in var.name]
 
+        # Loss and optimization
+        self._predicted_return = self._reward + self._gamma * tf.reduce_max(self._target_net, 1)
+        self._loss = tf.reduce_mean((self._predicted_return - (self._learning_net * self._action))**2.0)
+        self._optim = tf.train.AdamOptimizer(self._learning_rate).minimize(self._loss, var_list=self._learning_vars)
+
+        # Tensorflow init
         self._sess = tf.Session()
         self._sess.run(tf.global_variables_initializer())
 
